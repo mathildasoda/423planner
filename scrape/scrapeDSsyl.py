6f83549759eecb063170b17c554028bd156aecf1
hailie <hql23@drexel.edu>
2020-06-23 09:46:48 -0400

    div: degree reqs selected

--- scrape/scrapeDSsyl.py
+++ scrape/scrapeDSsyl.py
@@ -1,17 +1,25 @@
 
 import sys
-from urllib import request 
+import re
 from urllib.request import urlopen as uopen
+from urllib import parse, error
 from bs4 import BeautifulSoup as soup
 
 #datasci cred req url
 ds_url = "http://catalog.drexel.edu/undergraduate/collegeofcomputingandinformatics/datascience/#degreerequirementstext"
+#someone pls debuG THIS MF
 
 uclient = uopen(ds_url)
 ds_html = uclient.read()
 uclient.close()
 
 ds_soup = soup(ds_html, "html.parser")
-print(ds_soup.h1)
-print(ds_soup.h2)
-print(ds_soup.h3)
+print("""
+
+
+space
+
+
+""")
+ds_raw = ds_soup.find_all(id="degreerequirementstextcontainer")
+print(ds_raw)
