9dd1cfa64c0aba136b53a464710960f2c783534d
hailie <hql23@drexel.edu>
2020-06-23 12:29:59 -0400

    basic scraping headers, hours, codes

--- scrape/scrapeDSsyl.py
+++ scrape/scrapeDSsyl.py
@@ -1,6 +1,7 @@
 
 import sys
 import re
+from re import search as search
 from urllib.request import urlopen as uopen
 from urllib import parse, error
 from bs4 import BeautifulSoup as soup
@@ -14,12 +15,50 @@ ds_html = uclient.read()
 uclient.close()
 
 ds_soup = soup(ds_html, "html.parser")
+
+#debug space 
 print("""
+__________________
 
 
-space
+i dont wana blind
 
-
+__________________
 """)
-ds_raw = ds_soup.find_all(id="degreerequirementstextcontainer")
-print(ds_raw)
+
+ds_raw = ds_soup.find(id="degreerequirementstextcontainer")
+# print(ds_raw)
+
+# get headers by class="courselistcomment areaheader", remove html tags, print
+ds_headers = ds_soup.find_all("span", class_="courselistcomment areaheader")
+for i in ds_headers:
+    i = str(i.text)
+    s = re.findall("<.*?>",i)
+    for n in s:
+        i.remove(n,"")
+    if i!="":
+        print(i)
+
+
+# get credit hours by class="hourscol", remove html tags, print
+ds_hours = ds_soup.find_all("td", class_="hourscol")
+for i in ds_hours:
+    i = str(i.text)
+    s = re.findall("<.*?>",i)
+    for n in s:
+        i.remove(n,"")
+    if i!="":
+        print(i)
+
+
+# get course codes by class="hourscol", remove html tags, print
+ds_code = ds_soup.find_all("a", class_="bubblelink code")
+for i in ds_code:
+    i = str(i.text)
+    s = re.findall("<.*?>",i)
+    for n in s:
+        i.remove(n,"")
+    if i!="":
+        print(i)
+
+
